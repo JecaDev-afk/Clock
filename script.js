b313dd9e9bc47bcf8097bd7154667322f6c50eaf
Ярещенко Євгеній <evgenijaresenko24@email.com>
2025-10-19 19:06:34 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+
+function UpdateClock(){
+    // current time
+    const now = new Date();
+    const currentClockData = {
+
+        hours: now.getHours(),
+        minutes: now.getMinutes(),
+        seconds: now.getSeconds(),
+        
+        // month name
+        month: now.toLocaleString('en-US', {month: 'long'}),
+        weekDay: now.toLocaleDateString('en-US', {weekday: 'long'}),
+
+        day: now.getDate(),
+        year: now.getFullYear(),
+    }
+     displayTime(currentClockData);
+    
+}
+
+function displayTime(data){
+    const timeElement = document.getElementById("time")
+    const DateElement = document.getElementById("date")
+    // const WeekElement = document.getElementById("weekDay")
+
+    const formattedHour = data.hours.toString().padStart(2, '0');
+    const formattedMinute = data.minutes.toString().padStart(2, '0');
+    const formattedSecond = data.seconds.toString().padStart(2, '0');
+
+    const timeString = `${formattedHour} : ${formattedMinute} : ${formattedSecond}`
+    const dateString = `${data.day} : ${data.weekDay} : ${data.month} : ${data.year}`
+    // const weekString = `${data.weekDay}`
+    
+    timeElement.textContent = timeString;
+    DateElement.textContent = dateString;
+    // WeekElement.textContent = weekString;
+}
+
+UpdateClock()
+setInterval(UpdateClock, 1000);
+
+const themeSwitcher = document.getElementById("themeSwitcher");
+const body = document.body
+
+    themeSwitcher.addEventListener('click', function() {
+        // Code to be executed when the button is clicked
+       body.classList.toggle('lightTheme')
+    });
